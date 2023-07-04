9a191de24459752ec1b11ea6bbb451ed14d3ff75
Ryann Hally <134632565+ryannhally@users.noreply.github.com>
2023-07-04 18:56:52 -0400

    Update credentials.py

--- myspotifyproject/credentials.py
+++ myspotifyproject/credentials.py
@@ -1,10 +1,10 @@
 # My credentials
 
-CLIENT_ID = '1977ff0202a04b0b800a51861ec0cbc8'
+CLIENT_ID = ''
 
-CLIENT_SECRET = '153c5a637ef341e293c21e2e685b2573'
+CLIENT_SECRET = ''
 
-REDIRECT_URI = 'http://localhost:8888/'
+REDIRECT_URI = ''
 
-USERNAME = 'rrosele'
+USERNAME = ''
 
