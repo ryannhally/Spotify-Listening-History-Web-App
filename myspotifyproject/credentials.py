3201a1201f93546a722e00efa4f8a8866ef9c4f7
Ryann Hally <134632565+ryannhally@users.noreply.github.com>
2023-07-04 18:56:25 -0400

    Add files via upload

---
+++ myspotifyproject/credentials.py
@@ -0,0 +1,10 @@
+# My credentials
+
+CLIENT_ID = '1977ff0202a04b0b800a51861ec0cbc8'
+
+CLIENT_SECRET = '153c5a637ef341e293c21e2e685b2573'
+
+REDIRECT_URI = 'http://localhost:8888/'
+
+USERNAME = 'rrosele'
+
